65b35621ed795a82eb72e2e21869d94f87f42fea
Rizki Adi <rizkiadinugroho3@gmail.com>
2022-12-25 15:11:19 +0700

    added banner

---
+++ src/scripts/components/app-banner.js
@@ -0,0 +1,22 @@
+class AppBanner extends HTMLElement{
+  connectedCallback(){
+    this.bannerTitle = this.getAttribute('data-title');
+    this.ctaStatus = this.getAttribute('data-cta');
+    this.ctaText = this.getAttribute('data-cta-text');
+
+    this.render();
+  }
+
+  render(){
+    this.innerHTML=`
+      <div class="wrapper--wrap">
+        <h1 class="banner__title">${this.bannerTitle}</h1>
+        ${this.ctaStatus ? `
+          <a class="banner__button" href="#resto">${this.ctaText}</a>
+        ` : ''}
+      </div>
+    `;
+  }
+}
+
+customElements.define('app-banner', AppBanner);
